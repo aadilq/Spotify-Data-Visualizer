767e641cdb163796c2ac94c7f505571305d149b9
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-02-23 17:30:55 -0500

    "Integrated API"

--- script.js
+++ script.js
@@ -1,6 +1,104 @@
 // name file script.js
-import { songList } from './spotify_top_hits_clean_json.js';
-console.log(songList);
+import { songList } from "/spotify_top_hits_clean.js";
+
+const API_KEY = "BQBkS9tooGKCs1UupHNn6VZcur_ei-DAPvjYd-JuZ0W2kPEpdJQ7Itg2ctXJbRlM-m8koahDdWxOUUAwUN54EoA4E6ziFf_5hLy2h4u--OBW_7NZduf24WYrKOkLdL5W_-yrcPpiynQ"
+
+function showForm() { //displays the search entry bar
+    const resultCard = document.getElementById('result-card')
+    resultCard.innerHTML = ''
+    const artistSearch = document.createElement('div')
+    artistSearch.innerHTML =
+    `
+    <form id="artist-search-form">
+        <label for="artist-input">Search for an artist</label>
+        <input type="text" name="artist" id="artist-input" placeholder="Taylor Swift">
+        <button type="submit" id="submit-button">Search</button>
+    </form>
+    `
+    resultCard.prepend(artistSearch)
+    
+    const form = document.getElementById('artist-search-form')
+    form.addEventListener('submit', handleSubmit)
+}
+
+const handleSubmit = async (event) =>{
+    event.preventDefault();
+
+    const inputForm = document.getElementById('artist-input');
+    const artistRequested = inputForm.value;
+
+    const artists = await searchArtists(artistRequested)
+    // displayArtistCards(artists)
+}
+
+async function searchArtists(artist) { //Search for an artist using Spotify's API
+    const requestURL = `https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=5&offset=0`
+    const details = {
+        method: "GET", 
+        headers: {
+            Authorization: `Bearer ${API_KEY}`
+        }
+    }
+
+    const result = await fetch (requestURL, details)
+    const json = await result.json()
+    console.log(json)
+
+    const retrievedArtists = json.artists.items 
+    const cleanedArtistData = retrievedArtists.map(artists => (
+        {
+            id: artist.id,
+            name: artists.name,
+            link: artist.external_urls.spotify,
+            followers: artist.followers.total,
+            popularity: artist.popularity, 
+            genre: artist.genres, 
+            image: (artist.images.length > 0 ? artist.images[0].url : '')
+        }
+    ))
+
+    return cleanedArtistData
+
+}
+
+function displayArtistCards(artistDetails) {//displays the artists found to the screen
+
+    const resultCard = document.getElementById('result-card')
+    resultCard.innerHTML = ''
+    showForm()
+
+    console.log("Artist details : ", artistDetails)
+
+    const cardsContainer = document.createElement('div')
+    artistDetails.forEach((artist) => {
+        const card = document.createElement('div')
+        card.innerHTML = 
+            `
+            <div class="card-container">
+            <div class="detail-container">
+                <div class="img-container">
+                    <img src=${artist.image ? artist.image : 'https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png'} alt=${artist.name}/>
+                </div>
+                <div class="info-container">
+                    <h4>
+                        ${artist.name}
+                    </h4>
+                    <div>
+                        <span>${artist.genre} ◆ </span>
+                        <span>${artist.followers} Followers ◆ </span>
+                        <span>Popularity ${artist.popularity}</span>
+                    </div>
+                </div>
+            </div>
+            <h6 class="visit-link"><a href=${artist.link}>Visit Artist</a></h6>
+            </div>
+            `
+            
+           // card.addEventListener('click', () => {handleArtistClick(artist.id)})
+            cardsContainer.appendChild(card)
+        })
+        resultCard.append(cardsContainer)
+    }
 
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.result-button');
@@ -16,6 +114,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 createYearChart();
             } else if (buttonText === 'Loudest Song') {
                 createLoudestSongChart();
+            } else if(buttonText === 'Search'){
+                showForm();
             }
         });
     });
